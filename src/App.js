4004212b8966b50102c457eb47e720f4248e2d3c
[Tahiru Rafiu] <tahirurafiu61@gmail.com>
2024-02-04 11:25:43 +0000

    navigation added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import {Routes, Route} from "react-router-dom"
 import About from "./about/About";
 import Header from "./header/Header";
+import Portfolio from './portfolio/Portfolio'
+import Services from './services/Services'
+import Contact from './contact/Contact'
+import Navigation from "./navigation/Navigation";
 function App() {
   return (
     <div className="App">
       <Header />
-      <About />
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="portfolio" element={<Portfolio />} />
+        <Route path="services" element={<Services />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/navigation/Navigation.js
@@ -0,0 +1,49 @@
+import {Link} from 'react-router-dom'
+import React, {useEffect} from 'react'
+import './navigation.css'
+
+const Navigation = () => {
+  useEffect(() => {
+    const navbar = document.querySelector('.nav');
+    const navbarTop = navbar.offsetTop;
+
+    const adjustNavbar = () => {
+      if (window.scrollY > navbarTop) {
+        navbar.style.position = 'fixed';
+        navbar.style.top = '0';
+      } else {
+        navbar.style.position = 'static';
+      }
+    };
+
+    window.addEventListener('scroll', adjustNavbar);
+
+    return () => {
+      // Cleanup event listener on component unmount
+      window.removeEventListener('scroll', adjustNavbar);
+    };
+  }, []); // Empty dependency array ensures the effect runs only once on mount
+
+  return (
+    <>
+    <div className='nav'>
+      <nav>
+      <Link to='/' className='nav-item'>About Me</Link>
+      <Link to='portfolio' className='nav-item'>Portfolio</Link>
+      <Link to='services' className='nav-item'>Services</Link>
+      <Link to='contact' className='nav-item'>Contact</Link>
+      </nav>
+    </div>
+    <div className='mobileMenu'>
+      <section className='menu'>
+      <Link to='/' className='item'>About Me</Link>
+      <Link to='portfolio' className='item'>Portfolio</Link>
+      <Link to='services' className='item'>Services</Link>
+      <Link to='contact' className='item'>Contact</Link>
+      </section>
+      </div>
+    </>
+  )
+}
+
+export default Navigation;
